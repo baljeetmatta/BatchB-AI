dabbd93492cb644a023a9967edb0f0dbc9dfbd4e
Baljeet Matta <baljeetmatta@Baljeets-MacBook-Air.local>
2025-07-17 09:51:40 +0530

    Todo-completed

--- js/todo.js
+++ js/todo.js
@@ -17,7 +17,7 @@ const taskName = document.querySelector("#taskName");
 const taskContainer = document.querySelector("#taskContainer");
 const addBtn = document.querySelector("#addBtn");
 addBtn.addEventListener("click", function () {
-    addDom();
+    addTask();
 
 })
 
@@ -25,24 +25,34 @@ taskName.addEventListener("keyup", function (e) {
     // console.log(e);
     if (e.key == "Enter") {
         // tasks.push(taskName.value);
-        let task = {
-            id: taskid,
-            title: taskName.value,
-            status: "Pending"
-        }
-        tasks.push(task);
-        taskid++;
-
-
-        addDom(task);
-        taskName.value = "";
+        addTask();
     }
 
 
 })
+function addTask()
+{
+     let task = {
+            id: taskid,
+            title: taskName.value,
+            status: "Pending"
+        }
+        console.log(task);
+        
+        tasks.push(task);
+        taskid++;
+
+
+
+        addDom(task);
+        taskName.value = "";
+        storeTasks();
+}
 
 function addDom(task) {
     let mainDiv = document.createElement("div");
+    mainDiv.setAttribute("id",task.id);
+
 
     let titleSpan = document.createElement("span");
     titleSpan.innerText = task.title;
@@ -64,6 +74,7 @@ function addDom(task) {
             titleSpan.style.textDecoration="none";
 
         task.status=status;
+        storeTasks();
         console.log(tasks);
 
 
@@ -76,13 +87,15 @@ function addDom(task) {
     let delBtn=document.createElement("button");
     delBtn.innerText="Del";
 
-    delBtn.addEventListener("click",function(){
-        mainDiv.remove();
-       tasks= tasks.filter(function(item){
-            return item.id!=task.id;
-        })
-        console.log(tasks);
-    })
+    // delBtn.addEventListener("click",function(){
+    //     mainDiv.remove();
+    //    tasks= tasks.filter(function(item){
+    //         return item.id!=task.id;
+    //     })
+    //     console.log(tasks);
+    // })
+    delBtn.addEventListener("click",delHandler);
+
     mainDiv.appendChild(delBtn);
     
 
@@ -91,3 +104,51 @@ function addDom(task) {
 
 
 }
+function delHandler(e)
+{
+    let id=e.target.parentNode.getAttribute("id")
+e.target.parentNode.remove();
+tasks= tasks.filter(function(item){
+            return item.id!=id;
+        })
+        storeTasks();
+
+    //console.log(e.target.parentNode);
+
+}
+///Local Browser ->localStorage->localStorage
+
+//localStorage.setItem("A",10);
+//let arr=[10,20,30,40];
+//console.log(arr);
+//document.write(arr);
+let obj={
+    id:1,
+    "Name":"Text"
+}
+//console.log(obj);
+//document.write(JSON.stringify( obj));//Object ->String
+
+//JSON.parse()->String->Object
+function storeTasks()
+{
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+}
+function loadTasks()
+{
+    if(localStorage.getItem("tasks"))
+      tasks=JSON.parse( localStorage.getItem("tasks"));
+
+   tasks.forEach(function(task){
+    if(taskid<task.id)
+        taskid=task.id;
+    addDom(task)
+   })
+   if(tasks.length!=0)
+   taskid++;
+
+   //console.log(data[0].title);
+
+
+}
+loadTasks()
